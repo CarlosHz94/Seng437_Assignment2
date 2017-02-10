5dbfa10dd1377885592c613d1add35bebe54e80a
Carlos Ivan Hernandez Rosas <carlos_h_v18@hotmail.com>
2017-02-09 17:52:03 -0700

    Name testing and new test cases
    I changed the name of the tests we wrote during the lab, I also added
    some new test, there's a total of 17 test of which 6 failed. Most of the
    failures are due to overflow, However there's
    centralValueCloseToMaxRangeTest2 which uses values under the the max
    double values and it still fails.
    When picking the functions to test, make sure to read the documentaion
    in order to avoid creating tests due to misunderstandings like the prof
    mentioned today in class. (We thought of testing range by giving it
    reverse values new Range(1,-1) while the documentation specified that
    the first argument was the lower bound and the second was the upper
    bound where lower must be <= upper and upper must be >= lower)

--- RangeTest.java
+++ RangeTest.java
@@ -13,28 +13,110 @@ public class RangeTest{
 	
 	
 	@Test
-	public void centralValue0() {
+	public void simpleZeroCentralValueTest() {
 		exampleRange= new Range(-1,1);
 		assertEquals("The central value of -1 and 1 should 0", 0, exampleRange.getCentralValue(), .000000001d);
 	}
 	
 	@Test 
-	public void centralPositiveValue(){
+	public void centralPositiveValueTest(){
 		exampleRange = new Range(1,3);
 		assertEquals("The central value of 1 and 3 should be 2", 2, exampleRange.getCentralValue(), .000000001d);
 	}
 	
 	@Test
-	public void centralFloatingPointValue(){
+	public void centralNegativeValueTest(){
+		exampleRange = new Range(-3,-1);
+		assertEquals("The central value of -3 and -1 should be -2", -2, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	@Test
+	public void centralFloatingPointValueTest(){
 		exampleRange = new Range(1,2);
 		assertEquals("The central value of 1 and 2 should be 1.5", 1.5, exampleRange.getCentralValue(), .000000001d);
 	}
 	
 	@Test
-	public void centralMaxRangeTest(){
+	public void singleValueRangeCentralValueTest(){
+		exampleRange = new Range(1,1);
+		assertEquals("The central value of 1 and -1 should be 0", 1, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	@Test
+	public void centralValueMaxRangeTest(){
 		exampleRange = new Range(-Double.MAX_VALUE, Double.MAX_VALUE);
 		assertEquals("IDk", 0, exampleRange.getCentralValue(), .000000001d);
 	}
+	
+	@Test
+	public void centralValueOverMaxRangeTest(){
+		exampleRange = new Range(-Double.MAX_VALUE-1, Double.MAX_VALUE+1);
+		assertEquals("The central value of 1 extra from the max double value should be 0", 0, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	@Test
+	public void centralValueOverPositiveMaxRangeTest(){
+		exampleRange= new Range(-Double.MAX_VALUE, Double.MAX_VALUE+2);
+		assertEquals("The central value should be 1", 1, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	@Test
+	public void centralValueOverNegativeMaxRangeTest(){
+		exampleRange = new Range(-Double.MAX_VALUE-2, Double.MAX_VALUE);
+		assertEquals("The central value should be -1", -1, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	
+	@Test
+	public void centralValueCloseToMaxRangeTest(){
+		exampleRange = new Range(-Double.MAX_VALUE+1, Double.MAX_VALUE-1);
+		assertEquals("The central value should be 0", 0, exampleRange.getCentralValue(), .0000000001d);
+	}
+	
+	@Test
+	public void centralValueCloseToMaxRangeTest2(){
+		exampleRange = new Range(-Double.MAX_VALUE+3, Double.MAX_VALUE-1);
+		assertEquals("The central value should be 1", 1, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	@Test
+	public void centralValueMaxIntegerTest(){
+		exampleRange = new Range(-Integer.MAX_VALUE, Integer.MAX_VALUE);
+		assertEquals("The central value of max integer values should be 0", 0, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	@Test
+	public void centralValueOverMaxIntegerTest(){
+		exampleRange = new Range(-Integer.MAX_VALUE-1, Integer.MAX_VALUE+1);
+		assertEquals("The central value of the 1 extra over the max integer shoudl be 0", 0, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	@Test
+	public void centralValueOverPositiveIntegerMaxRangeTest(){
+		exampleRange = new Range(-Integer.MAX_VALUE, Integer.MAX_VALUE+2);
+		assertEquals("The central value of should be 1", 1, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	@Test
+	public void centralValueOverNegativeIntegerMaxRangeTest(){
+		exampleRange = new Range(-Integer.MAX_VALUE-2, Integer.MAX_VALUE);
+		assertEquals("The central value should be -1", -1, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+	
+	@Test
+	public void centralValueCloseToIntegerMaxRangeTest(){
+		exampleRange = new Range(-Integer.MAX_VALUE+1, Integer.MAX_VALUE-1);
+		assertEquals("The central value should be 0", 0, exampleRange.getCentralValue(), .0000000001d);
+	}
+	
+	@Test
+	public void centralValueCloseToIntegerMaxRangeTest2(){
+		exampleRange = new Range(-Integer.MAX_VALUE+3, Integer.MAX_VALUE-1);
+		assertEquals("The central value should be 1", 1, exampleRange.getCentralValue(), .000000001d);
+	}
+	
+
 
 	@After
 	public void tearDown()
