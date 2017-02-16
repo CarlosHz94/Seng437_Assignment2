2c6668293cde3fb8de872197788050f64bcbae0b
CarlosHz94 <carlos_h_v18@hotmail.com>
2017-02-15 23:10:04 -0700

    test cases for getUpperBound fucntion

--- src/org/jfree/data/test/DataUtilitiesTest.java
+++ src/org/jfree/data/test/DataUtilitiesTest.java
@@ -6,12 +6,14 @@ import org.jfree.data.DataUtilities;
 import org.jfree.data.Values2D;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.*;
 
 public class DataUtilitiesTest extends DataUtilities {
 	Mockery mockingContext;
 	Values2D values;
-
+	
 	@Before
 	public void setup(){
 		mockingContext = new Mockery();
@@ -23,11 +25,11 @@ public class DataUtilitiesTest extends DataUtilities {
 		//setup
 		mockingContext.checking(new Expectations() {
 			{
-				one(values).getRowCount();
+				oneOf(values).getRowCount();
 				will(returnValue(2));
-				one(values).getValue(0, 0);
+				oneOf(values).getValue(0, 0);
 				will(returnValue(7.5));
-				one(values).getValue(1, 0);
+				oneOf(values).getValue(1, 0);
 				will(returnValue(2.5));
 			}
 		});
@@ -35,9 +37,33 @@ public class DataUtilitiesTest extends DataUtilities {
 		double result = DataUtilities.calculateColumnTotal(values, 0);
 		// verify
 		assertEquals(result, 10.0, .000000001d);
-		// tear-down: NONE in this test method
-
-
-		}
+		// tear-down: NONE in this test method	
+	}
+	
+	@Test
+	public void calculateColumnTotalForTwoNegativeValues(){
+		mockingContext.checking(new Expectations(){
+			{
+				oneOf(values).getRowCount();
+				will(returnValue(2));
+				oneOf(values).getValue(0, 0);
+				will(returnValue(-5.6));
+				oneOf(values).getValue(1, 0);
+				will(returnValue(-4.4));
+			}
+		});
+		double result = DataUtilities.calculateColumnTotal(values, 0);
+		assertEquals(result, -10.0, .000000001d);
+	}
+	
+	@Test
+	public void calculateColumnTotalForCloseToMaxDoubleValues(){
+		mockingContext.checking(new Expectations(){
+			{
+				oneOf(values).getRowCount();
+				will()
+			}
+		});
+	}
 
 }
